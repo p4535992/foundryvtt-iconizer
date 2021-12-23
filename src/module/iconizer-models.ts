bc41b8bbdca8a1d0be0a8f2559bb91ccf2b4811e
p4535992 <tentimarco0@gmail.com>
2021-12-23 16:51:17 +0100

    update

---
+++ src/module/iconizer-models.ts
@@ -0,0 +1,13 @@
+export class VTTAIconizerLibrary {
+  iconData: any;
+
+  constructor(iconData: any) {
+    this.iconData = iconData;
+  }
+}
+
+export const VTTAIconizerItem = {
+  name: '',
+  type: '',
+  icon: '',
+};
